f0c8f5e3acd41df3d91297edf3905e3159f01546
Shashikant Manikonda <manikonda@outlook.com>
2025-12-13 16:51:23 -0500

    Fix test configuration and ignore runoutput directory

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,10 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+# Add src to path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
+
+# Check for sibling MTFLibrary and add to path if present
+mtflib_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../MTFLibrary/src"))
+if os.path.exists(mtflib_path):
+    sys.path.insert(0, mtflib_path)
